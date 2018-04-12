190ed9fa22e6a3a747cfc071940f552a806d5c78
小柒2012 <345849402@qq.com>
2018-04-12 09:27:57 +0800

    Quartz集群模式

--- src/main/java/com/itstyle/quartz/interceptor/MyAdapter.java
+++ src/main/java/com/itstyle/quartz/interceptor/MyAdapter.java
@@ -4,7 +4,8 @@ import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
- * 配置首页(在SpringBoot2.0及Spring 5.0 WebMvcConfigurerAdapter以被废弃 建议实现WebMvcConfigurer接口)
+ * 配置首页(在SpringBoot2.0及Spring 5.0 WebMvcConfigurerAdapter以被废弃 
+ * 建议实现WebMvcConfigurer接口)
  * 创建者 小柒2012
  * 创建时间  2018年4月10日
  */
